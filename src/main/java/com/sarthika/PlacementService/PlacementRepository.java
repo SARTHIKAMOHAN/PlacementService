0ef471adbba73b6f47288cdfdad7038b6dd1294d
USER <USER@LAPTOP-J4923RC4>
2023-09-19 16:08:10 +0530

    Placement service module

---
+++ src/main/java/com/sarthika/PlacementService/PlacementRepository.java
@@ -0,0 +1,7 @@
+package com.sarthika.PlacementService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlacementRepository extends JpaRepository<Placement, Integer>{
+
+}
